8c0880f7ab674c1a6023c95b60b66f383343c467
IT26101287 <IT26101287@my.sliit.lk>
2026-06-15 22:56:45 +0530

    add IT26101287Lab2Q3 to Q3

---
+++ Q3/IT26101287Lab2Q3.java
@@ -0,0 +1,14 @@
+public class IT26101287Lab2Q3{
+    public static void main(String[] args) {
+
+        double sideA = 3;
+        double sideB = 4;
+
+        double hypotenuse = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+
+        
+        System.out.println("Hypotenuse: " + hypotenuse);
+    }
+}
+
+
